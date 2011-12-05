89c563429f9bf49f6bacce0d6092231cb8e97749
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2011-12-05 17:02:46 +0200

    Issue #1359304 by andypost: Javascript followup.

--- js/draggableviews_list.js
+++ js/draggableviews_list.js
@@ -6,11 +6,11 @@
 (function ($) {
  Drupal.behaviors.draggableViews = {
   attach: function (context, settings) {
-    var list = $(".views-form ul." + Drupal.settings.draggableviews_row_class);
+    $('.views-form .' + Drupal.settings.draggableviews_row_class + ':not(.draggableviews-processed)', context)
     // Add class for theming.
-    list.addClass('sortable');
+    .addClass('draggableviews-processed')
     // Add sortable effect.
-    list.sortable({
+    .sortable({
       update: function(event, ui) {
         $( ".draggableviews-weight" ).each(function (i, Val) {
           $(this).val(i);
